4936bc972414d7144c459cdceb0b5401e8c4e59c
mhf <mhfcbz@163.com>
2018-04-11 23:04:17 +0800

    s:

--- icons/aa-index.js
+++ icons/aa-index.js
@@ -20,15 +20,18 @@ export function use(Vue, componentList) {
 import IconBase from "./aa-icon-base.vue"
 import IconMenu from "./menu.vue"
 import IconSearch from "./search.vue"
+import IconSquareBracket from "./square-bracket.vue"
 
 export {
   IconBase,
   IconMenu,
   IconSearch,
+  IconSquareBracket,
 }
 
 const components = [
   IconBase,
   IconMenu,
   IconSearch,
+  IconSquareBracket,
 ]
